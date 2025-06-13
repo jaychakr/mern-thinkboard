d25c8968366b9a374b881f1878652075d45d7b67
Jay Chakravarty <jaychakravarty@icloud.com>
2025-06-12 19:15:01 -0700

    change app.get to app.all

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ app.use("/api/notes", notesRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*", (req, res) => {
+    app.all("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
